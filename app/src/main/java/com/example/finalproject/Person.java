f01a807fc412e827f6fd0f25888bfbb566fb0d74
derrickgong <yijieg4@iliinois.edu>
2019-11-28 14:44:32 -0600

    final

---
+++ app/src/main/java/com/example/finalproject/Person.java
@@ -0,0 +1,46 @@
+package com.example.finalproject;
+
+public class Person {
+    /** weight of a person. */
+    private double weight;
+
+    /** height of a person. */
+    private double height;
+
+    /** age of a person. */
+    private int age;
+
+    /** construct a person object. */
+    Person(double setweight, double setheight, int setage) {
+        weight = setweight;
+        height = setheight;
+        age = setage;
+    }
+
+    /**
+     * calculate a person's BMI.
+     * @param weight weight
+     * @param height height
+     * @return BMI value.
+     */
+    public double getBMI(double weight, double height) {
+        double bmi = weight / (height * height);
+        return bmi;
+    }
+
+    /**
+     * get a person's weight status;
+     * @param bmi bmi index
+     * @return weight status.
+     */
+    public int weightstatus(double bmi) {
+        if (bmi < 18.5) {
+            return 0;
+        } else if (bmi >= 18.5 && bmi < 24.9) {
+            return 1;
+        } else if (bmi >= 24.9 && bmi < 29.9) {
+            return 2;
+        }
+        return 3;
+    }
+}
