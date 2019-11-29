5d7fdbd54808850537a38ba6c0327f51c20249dd
Ethanhuang17 <55036820+Ethanhuang17@users.noreply.github.com>
2019-11-29 16:50:58 -0600

    Update MainActivity.java

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -1,30 +1,50 @@
 package com.example.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    double weight;
+    double height;
+    int age;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button forinputdata = findViewById(R.id.data);
+        Button forstart = findViewById(R.id.start);
+        Intent intent = getIntent();
+        weight = intent.getDoubleExtra("weight", 0.0);
+        height = intent.getDoubleExtra("height", 0.0);
+        age = intent.getIntExtra("age", 0);
         forinputdata.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 opendraftdata();
             }
         });
-
+        forstart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openAnalysis();
+            }
+        });
     }
     public void opendraftdata() {
-        Intent intent = new Intent(this, Draftdata.class);
+        Intent intent = new Intent(this, draftdata.class);
         startActivity(intent);
-        finish();
     }
-
+    public void openAnalysis() {
+        Intent analysisintent = new Intent(this, Analysis.class);
+        analysisintent.putExtra("height", height);
+        analysisintent.putExtra("weight", weight);
+        analysisintent.putExtra("age", age);
+        startActivity(analysisintent);
+    }
 }
+
