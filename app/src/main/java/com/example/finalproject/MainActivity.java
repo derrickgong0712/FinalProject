4a5cda4ad9ea9457f3dc4ed9ea1a541c0d877c84
Ethanhuang17 <55036820+Ethanhuang17@users.noreply.github.com>
2019-11-30 16:38:26 -0600

    Update MainActivity.java

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -1,50 +1,84 @@
 package com.example.finalproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 
 public class MainActivity extends AppCompatActivity {
-    private double weight;
-    private double height;
-    private int age;
+    /* this is for Firebase.*/
+    private FirebaseAuth mAuth;
+    /* this is for the gameactivity.*/
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button forinputdata = findViewById(R.id.data);
-        Button forstart = findViewById(R.id.start);
-        Intent intent = getIntent();
-        weight = intent.getDoubleExtra("weight", 0.0);
-        height = intent.getDoubleExtra("height", 0.0);
-        age = intent.getIntExtra("age", 0);
-        forinputdata.setOnClickListener(new View.OnClickListener() {
+        intent = new Intent(this, draftdata.class);
+        Button signup = findViewById(R.id.button2);
+        Button forlogin = findViewById(R.id.login);
+        signup.setVisibility(View.VISIBLE);
+        FirebaseApp.initializeApp(this);
+        mAuth = FirebaseAuth.getInstance();
+        signup.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                opendraftdata();
+            public void onClick(View v) {
+                startTosignup();
             }
         });
-        forstart.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openAnalysis();
-            }
+        forlogin.setOnClickListener(v -> {
+            startActivity(intent);
         });
     }
-    public void opendraftdata() {
-        Intent intent = new Intent(this, Draftdata.class);
-        startActivity(intent);
-    }
-    public void openAnalysis() {
-        Intent analysisintent = new Intent(this, Analysis.class);
-        analysisintent.putExtra("height", height);
-        analysisintent.putExtra("weight", weight);
-        analysisintent.putExtra("age", age);
-        startActivity(analysisintent);
+    private void startTosignup() {
+        EditText foremail = findViewById(R.id.email);
+        EditText forpassword = findViewById(R.id.password);
+        mAuth.createUserWithEmailAndPassword(foremail.getText().toString(),
+                forpassword.getText().toString()).
+                addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    mAuth.getCurrentUser().sendEmailVerification().
+                            addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                if(mAuth.getCurrentUser().isEmailVerified()) {
+                                    startActivity(intent);
+                                }
+                                Toast.makeText(MainActivity.this,
+                                        "The link has been sent to your email!",
+                                        Toast.LENGTH_LONG).show();
+                                foremail.setText("");
+                                forpassword.setText("");
+                            } else {
+                                Toast.makeText(MainActivity.this,
+                                        task.getException().getMessage(),
+                                        Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    });
+                } else {
+                    Toast.makeText(MainActivity.this,
+                            task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
+
 
